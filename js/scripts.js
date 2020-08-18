a586eb028c903672a741f722f703997879497902
kccnma <gargiulo@hawaii.edu>
2020-08-18 13:37:43 -1000

    added accordians, but they are not accessible

--- js/scripts.js
+++ js/scripts.js
@@ -85,6 +85,13 @@ window.addEventListener('DOMContentLoaded', () => {
         observer3.observe(section);
     });
 
+    // ACCORDIANS
+    var accordians = document.querySelectorAll('.accordian');
+    for (var i = 0, len = accordians.length; i < len; i++) {
+        accordians[i].children[0].addEventListener('click', function () {
+            this.parentElement.classList.toggle('open');
+        });
+    };
 
 
 });
